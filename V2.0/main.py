85383bbc631e220be532cb62f298bd6d980dc4f8
gabriel@linux-romania.com <gabriel@linux-romania.com>
2020-05-03 06:44:00 +0000

    2020_05_03

--- V2.0/main.py
+++ V2.0/main.py
@@ -27,18 +27,32 @@ from kivy.lang import Builder
 from w1thermsensor import W1ThermSensor
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(5,  GPIO.OUT, initial=GPIO.HIGH) # 80 m relay
+GPIO.setup(6,  GPIO.OUT, initial=GPIO.HIGH) # 40 m relay
+GPIO.setup(13, GPIO.OUT, initial=GPIO.HIGH) # 20 m relay
+GPIO.setup(19, GPIO.OUT, initial=GPIO.HIGH) # 15 m relay
+GPIO.setup(26, GPIO.OUT, initial=GPIO.HIGH) # 10 m relay
+GPIO.setup(16, GPIO.OUT, initial=GPIO.HIGH) # ptt enable
+GPIO.setup(20, GPIO.OUT, initial=GPIO.HIGH) # power relay
+GPIO.setup(21, GPIO.OUT, initial=GPIO.HIGH) # reset protection
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) # ptt transmit indicator
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP) # input power overdrive
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Drain current alarm
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) # swr alarm
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) # filter alarm
 
-GPIO.setup(17,GPIO.IN, pull_up_down=GPIO.PUD_UP) # input power alarm
-GPIO.setup(5,  GPIO.OUT) # 80 m relay
-GPIO.setup(6,  GPIO.OUT) # 40 m relay
-GPIO.setup(13, GPIO.OUT) # 20 m relay
-GPIO.setup(19, GPIO.OUT) # 15 m relay
-GPIO.setup(26, GPIO.OUT) # 10 m relay
+GPIO.output(21,  0) # reset on
+sleep(0.1)
+GPIO.output(21,  1) # reset off
+
+
+
+
 
 def dallas(dummy,temp_1,speed):
     sensor = W1ThermSensor()
-    GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW) # PWM FAN_PIN
-    fan = GPIO.PWM(27, 25)
+    GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW) # PWM FAN_PIN
+    fan = GPIO.PWM(17, 25)
     fan.start(0)
     print 'Set fan'
     fan.ChangeDutyCycle(100)
@@ -65,14 +79,18 @@ class Main_Screen(FloatLayout):
         fwd_proc   = NumericProperty(0)
         ref_proc   = NumericProperty(0)
         drain_proc = NumericProperty(0)
+        ptt = StringProperty('Stand By')
         temp = NumericProperty(0)
         speed_1 = NumericProperty(0)
+        input_led = StringProperty('img/green-led.png')
         swr_led   = StringProperty('img/green-led.png')
         power_led = StringProperty("img/off.png")
         drain_led = StringProperty('img/green-led.png')
         filters_led = StringProperty('img/green-led.png')
         fault_status = False
         ht_bol = False
+        ref = 1.97 # pentru 100 W
+        #ref = 2.22 # pentru 1 kW
         power_text =StringProperty('[color=#008000]QRO Off[/color]')
         def home(self):
             if self.fault_status == False :
@@ -82,37 +100,43 @@ class Main_Screen(FloatLayout):
             if self.ht_bol == True:
                 self.power_text = '[color=#FF0000]QRO On[/color]'
                 self.power_led = "img/on.png"
-            else:
-                self.power_text = '[color=#008000]QRO Off[/color]'
+                GPIO.output(21,  0) # reset on
+                sleep(0.1)
+                GPIO.output(21,  1) # reset off
+                GPIO.output(20,  0) #power relay on
+                GPIO.output(16,  0) # ptt  ena relay on
+                self.ids._screen_manager.current = 'home'
+
+            if self.ht_bol == False :
                 self.power_led = "img/off.png"
+                GPIO.output(20,  1)    
+                GPIO.output(16,  1)
+                self.power_text = '[color=#008000]QRO Off[/color]'
+                self.ptt = '[size=20][b][color=#FFC800]Stand By[/color][/size][/b]'
 
         def swr(self):
-            if self.ref_proc > 50:
+            if  GPIO.input(23) == 0:
                 self.swr_led = 'img/red-led.png'
                 self.fault()
-                print self.ref_proc
+
             else:
                 self.swr_led = 'img/green-led.png'
-
         def input(self):
-            if (GPIO.input(17) == 0) :
-                self.swr_led = 'img/red-led.png'
+            if (GPIO.input(22) == 0) :
+                self.input_led = 'img/red-led.png'
                 self.fault()
-                print "pin 17 high"
             else:                
-                self.swr_led = 'img/green-led.png'
+                self.input_led = 'img/green-led.png'
         def drain(self):
-            if self.drain_proc > 50:
+            if  GPIO.input(27) == 0:
                 self.drain_led = 'img/red-led.png'
                 self.fault()
-                print "drain fault"
             else:
                 self.drain_led = 'img/green-led.png'
         def filters(self):
-            if self.fwd_proc > 50:
+            if  GPIO.input(24) == 0:
                 self.filters_led = 'img/red-led.png'
                 self.fault()
-                print "filters fault"
             else:
                 self.filters_led = 'img/green-led.png'
 
@@ -132,26 +156,40 @@ class Main_Screen(FloatLayout):
                 GPIO.output(5,  0)
             if band == 40:
                 GPIO.output(6,  0)                
-            if band == 200:
+            if band == 20:
                 GPIO.output(13, 0)                
             if band == 15:
                 GPIO.output(19, 0)
             if band == 10:
                 GPIO.output(26, 0)
+        def ptt_read(self,dummy):
+            if (GPIO.input(12) == 0 and self.ht_bol == True ) :
+                self.ptt =  '[size=20][b][color=#FF0600]Transmit[/color][/size][/b]'
+            if (GPIO.input(12) == 1 and self.ht_bol == True ) :
+                self.ptt = '[size=20][b][color=#00D53C]Receive[/color][/size][/b]'
+            if (self.ht_bol == False ) :
+                self.ptt = '[size=20][b][color=#FFC800]Stand By[/color][/size][/b]'
 
-
-
+            
 
         def update(self,*args):
-            self.fwd_proc = (fwd.value*100)/3.3
-            self.ref_proc = (ref.value*100)/3.3
+            s1 = 10 ** (   (self.ref - fwd.value)/0.025/10     )
+            self.fwd_proc = (1/s1)*100
+            
+            s2 = 10 ** (   (self.ref - ref.value)/0.025/10     )
+            self.ref_proc = (1/s2)*100
+            
             self.drain_proc  = (drain.value*100)/3.3
             self.temp = temp_1.value
             self.speed_1 = speed.value
+            self.ptt_read('dummy')
             self.swr()
             self.drain()
             self.filters()
             self.input()
+        
+
+            
 class MyApp(App):
       
     def build(self):
@@ -159,9 +197,13 @@ class MyApp(App):
         layout = FloatLayout()
         #Instantiate  UI objects ):
         main_screen = Main_Screen()
+        GPIO.add_event_detect(22, GPIO.BOTH, callback=main_screen.ptt_read)
+
         # Add the UI elements to the layout:
         layout.add_widget(main_screen)
+        Clock.schedule_once(main_screen.ptt_read)
         Clock.schedule_interval(main_screen.update, 0)
+
         return layout
 if __name__ == '__main__':
     temp_1 = Value('d',33)
